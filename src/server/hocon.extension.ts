5a73affd4e20bb49c94a3f2da6ac34d51b5e82a6
Yanbing Zhao <zzzz@mail.ustc.edu.cn>
2020-05-26 20:39:57 +0800

    Implement formatter

--- src/server/hocon.extension.ts
+++ src/server/hocon.extension.ts
@@ -3,12 +3,12 @@ import { Connection, createConnection, Diagnostic, DiagnosticSeverity, DocumentU
 import { TextDocument } from 'vscode-languageserver-textdocument'
 import { Node, NodeSequence } from './parser/basic'
 import { Problem, ProblemCollection } from './parser/problem'
-import { RootValue, rootValueRule } from './parser/sequencial'
+import { MultilineSpace, RootValue, rootValueRule } from './parser/sequencial'
 import { tokenizer } from './parser/tokenizer'
 
 const language: [string, string] = ['en', 'us']
 
-const astCollection: Map<DocumentUri, RootValue> = new Map()
+const astCollection: Map<DocumentUri, [RootValue, TextDocument]> = new Map()
 
 function* dfs(nodes: NodeSequence): Iterable<[Node | NodeSequence, number]> {
   const stack = [nodes.children[Symbol.iterator]()]
@@ -41,7 +41,7 @@ function refresh(connection: Connection, document: TextDocument): void {
     const message = messageCollection[language[0]] ?? messageCollection['en'] ?? 'UNRECOGNIZED_DOCUMENT'
     diagnostics.push({ range: { start, end }, source: 'hocon', code, message, severity: DiagnosticSeverity.Error })
   } else {
-    astCollection.set(document.uri, result)
+    astCollection.set(document.uri, [result, document])
     for (const [node] of dfs(result)) {
       if (node.type === 'problem') {
         const problem = node as Problem
@@ -64,9 +64,53 @@ function formatRange(connection: Connection, options: FormattingOptions, range: 
   return edits
 }
 
-function format(connection: Connection, options: FormattingOptions, document: TextDocumentIdentifier): TextEdit[] {
-  const edits: TextEdit[] = [], ast = astCollection.get(document.uri)
-  console.log(connection, options, ast) // TODO
+function format(connection: Connection, options: FormattingOptions, identifier: TextDocumentIdentifier): TextEdit[] {
+  const edits: TextEdit[] = [], astWithDocument = astCollection.get(identifier.uri)
+  if (astWithDocument !== undefined) {
+    const [ast, document] = astWithDocument
+    const indent = options.insertSpaces ? ' '.repeat(options.tabSize) : '\t'
+    const commentRegexp = /^(#|\/\/)(?!!)([\u00a0\u2007\u202f\ufeff]|[^\n\S])*/ // shebang
+    const stack: { level: number, indent: string, isHead: boolean, prevNewLineEdit?: TextEdit }[] = []
+    for (const [node, level] of dfs(ast.isWrapped ? ast : ast.body)) {
+      while ((stack[stack.length - 1]?.level ?? -1) >= level) stack.pop()
+      const lastIndex = stack.length - 1, peek = stack[lastIndex]
+      if (level - 1 === peek?.level) {
+        const newAccumulatedIndent = ''.concat(...stack.map(elem => elem.indent))
+        const accumulatedIndent = ''.concat(...stack.map((elem, index) => index === lastIndex ? '' : elem.indent))
+        if (node.type === 'multiline-space') for (const space of (node as MultilineSpace).children) {
+          if (space.type === 'new-line') {
+            if (peek.prevNewLineEdit !== undefined) peek.prevNewLineEdit.newText = newAccumulatedIndent
+            const start = document.positionAt(space.pos + space.raw.length), end = start
+            const edit = { range: { start, end }, newText: accumulatedIndent }
+            peek.indent = level > 1 ? indent : ''
+            peek.prevNewLineEdit = edit
+            peek.isHead = true
+            edits.push(edit)
+          } else if (space.type === 'comment') {
+            const start = document.positionAt(space.pos), end = document.positionAt(space.pos + space.raw.length)
+            const newText: string = space.raw.replace(commentRegexp, peek.isHead ? peek.indent + '$1 ' : ' $1 ')
+            edits.push({ range: { start, end }, newText })
+          } else {
+            const start = document.positionAt(space.pos), end = document.positionAt(space.pos + space.raw.length)
+            edits.push({ range: { start, end }, newText: '' })
+          }
+        } else {
+          if (node.type !== 'comma' && node.type !== 'colon') {
+            let firstChildNode = node
+            if (!peek.isHead) {
+              while (firstChildNode.raw === undefined) firstChildNode = firstChildNode.children[0]
+              const start = document.positionAt(firstChildNode.pos), end = start
+              edits.push({ range: { start, end }, newText: ' ' })
+            }
+          }
+          peek.isHead = false
+        }
+      }
+      if (['root-value', 'object-body', 'list-body', 'object-field'].indexOf(node.type) > 0) {
+        stack.push({ level, indent: '', isHead: true })
+      }
+    }
+  }
   return edits
 }
 
